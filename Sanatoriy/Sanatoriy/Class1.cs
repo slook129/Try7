384f415e1555c29a7f9d49e047ed80e982f2ffad
Ivan <ivankrivonogov1823@mail.ru>
2022-12-19 21:52:55 +0400

    Создание полей класса Услуги

--- Sanatoriy/Sanatoriy/Class1.cs
+++ Sanatoriy/Sanatoriy/Class1.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Sanatoriy
 {
-    class Class1
+    class Service
     {
+        public string Schedule { get { return schedule; } set { Schedule = value; } }
+        public string Price { get { return price; } set { peice = value; } }
+        public string Desription { get { return description; } set { description = value; } }
+
+
     }
 }
