fff9a991b58418dbdb1ea30f8e96093fd18bb0b6
slook129 <slookjs129@gmail.com>
2022-12-19 21:36:14 +0400

    Реализован интерфейс IComparable для класса Guest

--- Sanatoriy/Sanatoriy/Guest.cs
+++ Sanatoriy/Sanatoriy/Guest.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace Sanatoriy
 {
-    public class Guest
+    public class Guest:IComparable
     {
         private string role;
         private string medpolic;
         public string Role { get { return role; }  set { role = value; } }
         public string Medpolic { get { return medpolic; } set { medpolic = value; } }
+
+        public int CompareTo(object? obj)
+        {
+            Guest comparing = obj as Guest;
+            return comparing.Medpolic.CompareTo(this.Medpolic);
+        }
     }
 }
