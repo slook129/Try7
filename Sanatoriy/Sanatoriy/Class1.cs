4a88fb79eff53418edc9264b30aaa0924246fe6d
Ivan <ivankrivonogov1823@mail.ru>
2022-12-19 22:14:13 +0400

    Исправление ошибок в комментариях коментариев в классе Услуга

--- Sanatoriy/Sanatoriy/Class1.cs
+++ Sanatoriy/Sanatoriy/Class1.cs
@@ -8,12 +8,12 @@ namespace Sanatoriy
 {
     class Service
     {
-        // Поля класса услуги.
+        // Поля класса услуга.
         public string Schedule { get { return schedule; } set { Schedule = value; } }
         public string Price { get { return price; } set { peice = value; } }
         public string Desription { get { return description; } set { description = value; } }
         /// <summary>
-        /// //Конструктор класса услуги, который создает элементы класса Услуги.
+        /// //Конструктор класса услуга, который создает элементы класса Услуга.
         /// </summary>
         public Service()
         {
@@ -21,8 +21,6 @@ namespace Sanatoriy
             price = "";
             desription = "";
         }
-        
-        
-
+       
     }
 }
