1c6f14dc37e246b33c47f9e2f4d67c74a8a46485
EgorNa <egrghost15@gmail.com>
2022-12-19 22:04:49 +0400

    Написаны коментарии к классу Пользователь

--- Sanatoriy/Sanatoriy/UserClass.cs
+++ Sanatoriy/Sanatoriy/UserClass.cs
@@ -16,11 +16,19 @@ namespace Sanatoriy
         public string Password { get { return password; } set { password = value; } }
         string fullName;
         public string FullName { get { return fullName; } set { fullName = value; } }
+        /// <summary>
+        /// Метод, сравнивающий пароли у двух пользователей 
+        /// </summary>
+        /// <param name="obj"> - Объект, который потом сравнивается с нашим пользователем</param>
+        /// <returns>Числа: -1, 0, 1, в зависимости от выполненных условий</returns>
         public int CompareTo(object? obj)
         {
             User comparing = obj as User;
             return comparing.Password.CompareTo(this.Password);
         }
+        /// <summary>
+        /// Конструктор, создающий элемент класса User
+        /// </summary>
         public User()
         {
             email = "";
