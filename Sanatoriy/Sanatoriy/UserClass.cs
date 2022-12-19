b532cd0eb8f6a054fa0b971bc9112203fffa2650
EgorNa <egrghost15@gmail.com>
2022-12-19 21:40:50 +0400

    Создание полей в классе Пользователь

--- Sanatoriy/Sanatoriy/UserClass.cs
+++ Sanatoriy/Sanatoriy/UserClass.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Sanatoriy
 {
-    class UserClass
+    class User
     {
-
+        string email;
+        public string Email { get { return email; } set { email = value; } }
+        string phone;
+        public string Phone { get { return phone; } set { phone = value; } }
+        string password;
+        public string Password { get { return password; } set { password = value; } }
+        string fullName;
+        public string FullName { get { return fullName; } set { fullName = value; } }
     }
 }
