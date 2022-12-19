7f510610ce69a2c2ecf9992e58e716a89ecee4ca
Ivan <ivankrivonogov1823@mail.ru>
2022-12-19 22:04:05 +0400

    Написание коментариев в классе Услуги

--- Sanatoriy/Sanatoriy/Class1.cs
+++ Sanatoriy/Sanatoriy/Class1.cs
@@ -8,16 +8,20 @@ namespace Sanatoriy
 {
     class Service
     {
+        // Поля класса услуги.
         public string Schedule { get { return schedule; } set { Schedule = value; } }
         public string Price { get { return price; } set { peice = value; } }
         public string Desription { get { return description; } set { description = value; } }
-
+        /// <summary>
+        /// //Конструктор класса услуги, который создает элементы класса Услуги.
+        /// </summary>
         public Service()
         {
             schedule = "";
             price = "";
             desription = "";
         }
+        
         
 
     }
